7c8d22960929d74639850168d8bea6838c4496bb
Moritz <git@mrtz.me>
2017-09-20 07:47:21 -0400

    Add command to export dumpsters as geojson

---
+++ dumpstermap-backend/dumpsters/management/commands/export.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from dumpsters.serializers import DumpsterSerializer
+from dumpsters.models import Dumpster
+
+class Command(BaseCommand):
+    help = 'Exports dumpsters as geojson'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        filename = options['file'][1]
+        dumpsters = Dumpster.objects.all()
+        serializer = DumpsterSerializer(dumpsters, many=True)
+        from rest_framework.renderers import JSONRenderer
+
+        json = JSONRenderer().render(serializer.data)
+        out = open(filename, 'w')
+        out.write(json)
+        out.close()
+
+
+
