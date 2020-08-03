a14494d550caf9e9315bb112631eb27ab2d81a28
Moritz Duchêne <git@mrtz.me>
2020-08-03 23:05:23 +0200

    Retrieve database settings from DATABASE_URL

--- dumpstermap/settings/production.py
+++ dumpstermap/settings/production.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from .base import *
 import os
 
@@ -11,11 +13,5 @@ ALLOWED_HOSTS = ['dumpstermap.org',
 STATIC_URL = '/static/'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
-    }
+    'default': dj_database_url.config()
 }
