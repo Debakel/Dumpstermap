086dde2f790a58991e1110b60a195ad8cb9fb02f
Moritz Duchêne <2857237+Debakel@users.noreply.github.com>
2021-10-12 14:44:28 +0200

    Add test for dumpster list view

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,16 @@
-def test_slippy_map_view():
-    # TODO
-    pass
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from tests.factories import DumpsterFactory
+
+
+def test_dumpsters_list(db):
+    dumpster = DumpsterFactory()
+
+    response = APIClient().get('/dumpsters/')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    assert response.data["type"] == "FeatureCollection"
+    assert len(response.data["features"]) == 1
+    assert response.data["features"][0]["id"] == dumpster.id
