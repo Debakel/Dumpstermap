b62f5f0b3a407d5ff60536121e34e92072a1b5db
Moritz Duchêne <2857237+Debakel@users.noreply.github.com>
2022-01-23 14:20:59 +0100

    docs: Add types to tilenum2deg()

--- dumpsters/geo.py
+++ dumpsters/geo.py
@@ -1,7 +1,8 @@
 import math
+from django.contrib.gis import geos
 
 
-def tilenum2deg(x: float, y: float, zoom: int):
+def tilenum2deg(x: float, y: float, zoom: int) -> geos.Point:
     """Returns the coordinates of a slippy map tile
 
     * Returns the NW-corner of the square
@@ -14,4 +15,4 @@ def tilenum2deg(x: float, y: float, zoom: int):
     lon_deg = x / n * 360.0 - 180.0
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
     lat_deg = math.degrees(lat_rad)
-    return lat_deg, lon_deg
+    return geos.Point(lat_deg, lon_deg)
