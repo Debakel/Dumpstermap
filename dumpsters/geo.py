3f7d9a694edfe54c1fccabbd23f105bec620ccf0
Moritz Duchêne <2857237+Debakel@users.noreply.github.com>
2022-01-23 13:56:33 +0100

    docs: Update docstring for tilenum2deg()

--- dumpsters/geo.py
+++ dumpsters/geo.py
@@ -1,14 +1,17 @@
 import math
 
 
-#
-# Converts Tile numbers to lon./lat. (for slippy maps)
-# This returns the NW-corner of the square. Use the function with xtile+1 and/or ytile+1 to get the other corners.
-# With xtile+0.5 & ytile+0.5 it will return the center of the tile.
-# See https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Python
-def tilenum2deg(xtile, ytile, zoom):
+def tilenum2deg(x: float, y: float, zoom: int):
+    """Returns the coordinates of a slippy map tile
+
+    * Returns the NW-corner of the square
+    * Use with x+1 and/or y+1 to get the other corners.
+    * With x+0.5 & y+0.5 it will return the center of the tile.
+
+    See https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Python
+    """
     n = 2.0 ** zoom
-    lon_deg = xtile / n * 360.0 - 180.0
-    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lon_deg = x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
     lat_deg = math.degrees(lat_rad)
-    return (lat_deg, lon_deg)
+    return lat_deg, lon_deg
