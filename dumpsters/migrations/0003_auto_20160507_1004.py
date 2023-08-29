69c91a696ceabb4143dafbf26f4327a03ef11932
Moritz Duchêne <mail@mrtz.me>
2023-08-29 13:34:16 +0200

    Bump pre-commit hooks (#157)

--- dumpsters/migrations/0003_auto_20160507_1004.py
+++ dumpsters/migrations/0003_auto_20160507_1004.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("dumpsters", "0002_dumpster_location"),
     ]
--- dumpsters/migrations/0005_auto_20160510_1810.py
+++ dumpsters/migrations/0005_auto_20160510_1810.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("dumpsters", "0004_auto_20160508_0832"),
     ]
