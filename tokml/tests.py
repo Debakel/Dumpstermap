c11289643d1b4c2339a317a9df449f0c6c9983b7
Moritz Duchêne <2857237+Debakel@users.noreply.github.com>
2024-01-15 18:10:09 +0100

    fix: not recognized as a supported file format error
    Fixes #193

--- tokml/tests.py
+++ tokml/tests.py
@@ -1,8 +1,30 @@
+import os
+from string import Template
+from tempfile import NamedTemporaryFile
+
 import tokml
 
 
-def test_tokml() -> None:
-    assert kml_string == tokml.to_string(geojson_dict)
+def test_to_file():
+    """Ensure exporting a GeoJSON object to a KML file works"""
+    # GIVEN
+    file = NamedTemporaryFile(suffix=".kml", mode="r")
+
+    # WHEN
+    tokml.to_file(geojson_dict, file.name)
+
+    # THEN
+    content = file.read()
+    kml_folder_name = os.path.basename(file.name).split(".kml")[0]
+    assert content == expected_kml_string.substitute(name=kml_folder_name)
+
+
+def test_to_string():
+    """Ensure exporting a GeoJSON object to a KML string works"""
+    assert "<name>The Plaice to Be</name>" in tokml.to_string(geojson_dict)
+    assert expected_kml_string.substitute(name="Lorem Ipsum") == tokml.to_string(
+        geojson_dict, folder_name="Lorem Ipsum"
+    )
 
 
 geojson_dict = {
@@ -31,10 +53,11 @@ geojson_dict = {
         },
     ],
 }
-kml_string = """<?xml version="1.0" encoding="utf-8" ?>
+expected_kml_string = Template(
+    """<?xml version="1.0" encoding="utf-8" ?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document id="root_doc">
-<Folder><name>Places</name>
+<Folder><name>$name</name>
   <Placemark>
 	<name>The Plaice to Be</name>
 	<description>Traditional fish &amp; chips, and more, all cooked to order.</description>
@@ -48,3 +71,4 @@ kml_string = """<?xml version="1.0" encoding="utf-8" ?>
 </Folder>
 </Document></kml>
 """
+)
